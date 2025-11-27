6c54e4ab52926427a7a64d936ee28f403f6960c3
Ermel-06 <ermelcorpus362@gmail.com>
2025-11-27 14:35:17 +0800

    All Done

---
+++ data.js
@@ -0,0 +1,71 @@
+universe7 = [
+    {
+        name: "Goku",
+        image: "images/Goku.png",
+        description: "The Saiyan warrior with an insatiable appetite for battle and food.",
+    },
+    {
+        name: "Vegeta",
+        image: "images/Vegeta.png",
+        description: "The proud Saiyan prince known for his fierce rivalry with Goku."
+    },
+    {
+        name: "Gohan",
+        image: "images/Gohan.png",
+        description: "Goku's eldest son, a scholar and a powerful fighter when needed."
+    },
+    {
+        name: "Frieza",
+        image: "images/Frieza.png",
+        description: "The tyrannical overlord who reluctantly joined forces with Universe 7."
+    },
+    {
+        name: "Piccolo",
+        image: "images/Piccolo.png",
+        description: "The wise Namekian warrior and mentor to Gohan."
+    },
+    {
+        name: "Krillin",
+        image: "images/Krillin.png",
+        description: "Goku's best friend and a skilled martial artist."
+    },
+    {
+        name: "Android 18",
+        image: "images/Android18.png",
+        description: "A former enemy turned ally, known for her strength and wit."
+    },
+    {
+        name: "Android 17",
+        image: "images/Android17.png",
+        description: "A cool and collected warrior with a strong sense of justice."
+    },
+    {
+        name: "Master Roshi",
+        image: "images/MasterRoshi.png",
+        description: "The legendary martial arts master with a quirky personality."
+    },
+    {
+        name: "Tien Shinhan",
+        image: "images/Tien.png",
+        description: "A disciplined fighter known for his unique techniques and dedication."
+    },
+];
+
+Leader = [
+    {
+        name: "God of Destruction, Beerus",
+        image: "images/Beerus.png",
+        description: "The God of Destruction with immense power and a laid-back attitude."
+    },
+    {
+        name: "Angel, Whis",
+        image: "images/Whis.png",
+        description: "The angelic attendant and mentor to Lord Beerus."
+    },
+    {
+        name: "Supreme Kai, Kaio-Shin",
+        image: "images/Shin.png",
+        description: "The Supreme Kai who oversees Universe 7 alongside the God of Destruction."
+    }
+
+];
